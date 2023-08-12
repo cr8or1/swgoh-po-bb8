fa63691b6747388a1d00670c0aee7c65a08c00f4
Creator <35141845+cr8or1@users.noreply.github.com>
2023-08-12 11:35:57 +0300

    Improvements: - [x] Update the bot to the latest npm versions (discord js) - [x] THe bot now uses CDN Json instead of local (no need to rebuild on the json modifications) - [x] Release version 2 of the bot

--- bot.js
+++ bot.js
@@ -1,110 +1,91 @@
 // Init project
+const axios = require('axios');
 const Discord = require("discord.js");
-const fs = require("fs");
 
-const client = new Discord.Client();
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+    ],
+});
 
-// Channel discord id 1 (squad arena)
-
-var writeChannelSquadShard;
-var writeChannelFleetShard;
-var writeChannelSquadPest;
-var writeChannelFleetPest;
-var messageSquadShard;
-var messageFleetShard;
-var messageSquadPest;
-var messageFleetPest;
-
-// Parse a JSON data file
-const matesSquadShardData = parseData(JSON.parse(fs.readFileSync("./po-squad-shard-data.json", "utf8")));
-const matesFleetShardData = parseData(JSON.parse(fs.readFileSync("./po-fleet-shard-data.json", "utf8")));
-const matesSquadPestData = parseData(JSON.parse(fs.readFileSync("./po-squad-pest-data.json", "utf8")));
-const matesFleetPestData = parseData(JSON.parse(fs.readFileSync("./po-fleet-pest-data.json", "utf8")));
+// Initialize channel and message objects
+const channelMessagePairs = [
+    { channelId: process.env.channelIdSquadShard, message: null },
+    { channelId: process.env.channelIdFleetShard, message: null },
+    { channelId: process.env.channelIdSquadPest, message: null },
+    { channelId: process.env.channelIdFleetPest, message: null }
+];
 
 // Keeping the project "alive"
 setInterval(() => {
-	console.log(new Date().toISOString().replace("T", " ").substring(0, 19) + " Reinitializing the bot");
-	main().catch(ex => console.error(ex.message));
+    console.log(new Date().toISOString().replace("T", " ").substring(0, 19) + " Reinitializing the bot");
+    main().catch(ex => console.error(ex.message));
 }, process.env.timePeriod);
 
-
-// Initialize the bot
 client.on("ready", async () => {
-    client.user.setPresence({game: {name: "live payout countdowns", type: 0}});
-    writeChannelSquadShard = await client.channels.fetch(process.env.channelIdSquadShard);
-    writeChannelFleetShard = await client.channels.fetch(process.env.channelIdFleetShard);
-    writeChannelSquadPest = await client.channels.fetch(process.env.channelIdSquadPest);
-    writeChannelFleetPest = await client.channels.fetch(process.env.channelIdFleetPest);
+    client.user.setPresence({ activity: { name: "live payout countdowns", type: "WATCHING" } });
+
+    // Initialize channel and message objects
+    for (const pair of channelMessagePairs) {
+        const writeChannel = await client.channels.fetch(pair.channelId);
+        console.log(`Channel ${pair.channelId} fetched`);
+        pair.message = await initializeMessageObject(writeChannel);
+    }
 
     // Initial call
     await main();
 });
+
 client.login(process.env.botToken);
 
-console.log("App restarted version 1.0.0");
+console.log("App restarted version 2.0.0");
 console.log(process.env.url);
 
 async function main() {
-    if (!messageSquadShard) {
-        messageSquadShard = await initializeMessageObject(writeChannelSquadShard);
-    }
-    if (!messageFleetShard) {
-        messageFleetShard = await initializeMessageObject(writeChannelFleetShard);
-    }
-    if (!messageSquadPest) {
-        messageSquadPest = await initializeMessageObject(writeChannelSquadPest);
-    }
-    if (!messageFleetPest) {
-        messageFleetPest = await initializeMessageObject(writeChannelFleetPest);
-    }
-
-    if (messageSquadShard) {
-        await sendToChannel(matesSquadShardData, writeChannelSquadShard, messageSquadShard);
-    } else {
-        console.error("Shard message object not initialized");
-    }
-
-    if (messageFleetShard) {
-        await sendToChannel(matesFleetShardData, writeChannelFleetShard, messageFleetShard);
-    } else {
-        console.error("Shard message object not initialized");
-    }
-
-    if (messageSquadPest) {
-        await sendToChannel(matesSquadPestData, writeChannelSquadPest, messageSquadPest);
-    } else {
-        console.error("Pest message object not initialized");
-    }
-
-    if (messageFleetPest) {
-        await sendToChannel(matesFleetPestData, writeChannelFleetPest, messageFleetPest);
-    } else {
-        console.error("Pest message object not initialized");
-    }
-}
-
-// Below are the rest of the functions that make up the bot
-async function sendToChannel(mates, writeChannel, message) {
     try {
-        calculateSecondsUntilPayout(mates);
-        await sendMessage(mates, message);
-    } catch (err) {
-        console.log(err);
+        for (const pair of channelMessagePairs) {
+            if (!pair.message) {
+                console.error(`Message object for channel ${pair.channelId} not initialized`);
+                continue; // Skip this iteration and move to the next channel
+            }
+
+            const url = determineUrl(pair.channelId);
+            if (!url) {
+                console.error(`URL not found for channel ${pair.channelId}`);
+                continue; // Skip this iteration and move to the next channel
+            }
+
+            const response = await axios.get(url);
+            const matesData = parseData(response.data);
+
+            if (matesData) {
+                calculateSecondsUntilPayout(matesData);
+                await sendMessage(matesData, pair.message); // Note: No need for 'writeChannel' argument
+
+                console.log(`Message sent for channel: `, pair.channelId);
+            } else {
+                console.error(`Data not available for channel ${pair.channelId}`);
+            }
+        }
+    } catch (error) {
+        console.error("Error in main function:", error);
     }
 }
+
 
 async function initializeMessageObject(writeChannel) {
-    // fetch message, create a new one if necessary
     console.log('Start initializing message object');
     try {
         const messages = await writeChannel.messages.fetch();
 
-        if (messages.array().length === 0) {
-            return await writeChannel.send({embed: new Discord.MessageEmbed()});
+        if (messages.size === 0) {
+            return await writeChannel.send({embed: new Discord.EmbedBuilder()});
         } else {
             if (messages.first().embeds.length === 0) {
                 await messages.first().delete();
-                return await writeChannel.send({embed: new Discord.MessageEmbed()});
+                return await writeChannel.send({embed: new Discord.EmbedBuilder()});
             } else {
                 return messages.first();
             }
@@ -122,12 +103,16 @@ function parseData(shardData) {
 
     for (let i in shardData) {
         const user = shardData[i];
+        const poHours = parseInt(user.UTC.substr(0, 2));
+        const poMinutes = parseInt(user.UTC.substr(-2, user.UTC.length));
+        const payout = parseInt(user.UTC.substr(0, 2) + user.UTC.substr(-2, user.UTC.length));
+
         mates.push({
             name: user.Name,
-            payout: parseInt(user.UTC.substr(0, 2) + user.UTC.substr(-2, user.UTC.length)),
+            payout: payout,
             po: {
-                hours: parseInt(user.UTC.substr(0, 2)),
-                minutes: parseInt(user.UTC.substr(-2, user.UTC.length))
+                hours: poHours,
+                minutes: poMinutes
             },
             flag: user.Flag,
             swgoh: user.SWGOH,
@@ -136,6 +121,7 @@ function parseData(shardData) {
     }
 
     const matesByTime = {};
+
     for (let i in mates) {
         const mate = mates[i];
         if (!matesByTime[mate.payout]) {
@@ -152,41 +138,70 @@ function parseData(shardData) {
 
 function calculateSecondsUntilPayout(mates) {
     const now = new Date();
+
     for (let i in mates) {
         const mate = mates[i];
         const p = new Date();
-        p.setUTCHours(mate.po.hours, mate.po.minutes, 0, 0);
-        if (p < now) p.setDate(p.getDate() + 1);
-        mate.timeUntilPayout = p.getTime() - now.getTime();
-        let dif = new Date(mate.timeUntilPayout);
-        const round = dif.getTime() % 60000;
-        if (round < 30000) {
-            dif.setTime(dif.getTime() - round);
+
+        // Add this line to check if 'mate.po' exists before accessing 'hours'
+        if (mate.po) {
+            p.setUTCHours(mate.po.hours, mate.po.minutes, 0, 0);
+            if (p < now) p.setDate(p.getDate() + 1);
+            mate.timeUntilPayout = p.getTime() - now.getTime();
+            let dif = new Date(mate.timeUntilPayout);
+            const round = dif.getTime() % 60000;
+            if (round < 30000) {
+                dif.setTime(dif.getTime() - round);
+            } else {
+                dif.setTime(dif.getTime() + 60000 - round);
+            }
+            mate.time = `${String(dif.getUTCHours()).padStart(2, '00')}:${String(dif.getUTCMinutes()).padStart(2, '00')}`;
         } else {
-            dif.setTime(dif.getTime() + 60000 - round);
+            console.log('calculateSecondsUntilPayout mate.po is undefined:', mate);
         }
-        mate.time = `${String(dif.getUTCHours()).padStart(2, '00')}:${String(dif.getUTCMinutes()).padStart(2, '00')}`;
     }
 
     mates.sort((a, b) => {
         return a.timeUntilPayout - b.timeUntilPayout;
-    })
+    });
 }
 
 async function sendMessage(mates, message) {
-    let embed = new Discord.MessageEmbed().setThumbnail(process.env.thumbnail);
-    let desc = '**Time until next payout**:';
+    const embed = new EmbedBuilder().setThumbnail(process.env.thumbnail);
+    const desc = '**Time until next payout**:';
+
     for (let i in mates) {
         let fieldName = "\n" + "------------------" + "\n" + "PO in " + String(mates[i].time) + " - (UTC " + String(mates[i].po.hours).padStart(2, '00') + ":" + String(mates[i].po.minutes).padStart(2, '00') + "):";
         let fieldText = '';
         for (const mate of mates[i].mates) {
             fieldText += `${mate.flag} [${mate.name}](${mate.swgoh})\n`; // Discord automatically trims messages
         }
-        embed.addField(fieldName, fieldText, true);
+        embed.addFields({name: fieldName, value: fieldText, inline: true});
     }
+
     embed.setDescription(desc);
-    embed.setFooter('Last refresh', process.env.thumbnail);
+    embed.setFooter({
+        text: 'Last refresh',
+        iconURL: process.env.thumbnail,
+    });
     embed.setTimestamp();
-    await message.edit({embed});
-    console.log('Message send');
+
+    return await message.edit({ embeds: [embed] });
+}
+
+
+// Fetch data from appropriate URLs based on the channel ID
+function determineUrl(channelId) {
+    switch (channelId) {
+        case process.env.channelIdSquadShard:
+            return 'https://raw.githubusercontent.com/cr8or1/swgoh-po-bb8/main/po-squad-shard-data.json';
+        case process.env.channelIdFleetShard:
+            return 'https://raw.githubusercontent.com/cr8or1/swgoh-po-bb8/main/po-fleet-shard-data.json';
+        case process.env.channelIdSquadPest:
+            return 'https://raw.githubusercontent.com/cr8or1/swgoh-po-bb8/main/po-squad-pest-data.json';
+        case process.env.channelIdFleetPest:
+            return 'https://raw.githubusercontent.com/cr8or1/swgoh-po-bb8/main/po-fleet-pest-data.json';
+        default:
+            return '';
+    }
 }
